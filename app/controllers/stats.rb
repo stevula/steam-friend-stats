3b4ffcc1f0c055cf286217610015420c0aedcd52
Steven Broderick <brodericksteven@gmail.com>
2015-12-17 12:36:05 -0800

    Rewrite stats controller for SteamID - non-functional

--- app/controllers/stats.rb
+++ app/controllers/stats.rb
@@ -1,6 +1,5 @@
 get '/stats' do
-  steam_id = SteamId.community_id_to_steam_id(params[:communityID].to_i)
-  user = SteamId.from_steam_id(steam_id)
+  user = SteamId.new(params[:steam_id].to_i)
 
   erb :'stats/_stats', layout: false, locals: {user: user}
 end
